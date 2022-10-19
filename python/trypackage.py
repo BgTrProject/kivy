0915a644a89fef69b9759db0a5a8ef15d7fbef25
BgTrProject <85813523+BgTrProject@users.noreply.github.com>
2022-10-19 15:24:35 +0300

    Update trypackage.py

--- python/trypackage.py
+++ python/trypackage.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 def controlpackage():
-    for package in ['kivy','kivymd', 'pandas', 'tweepy', 'dtale', 'templates', 'snscrape','credentials','pyplotlib<3.3.0','requests_html','dateparser','demjson3','selenium','environ']:
+    for package in ['kivy','kivymd', 'webdriver_manager','pandas','unidecode','bs4','fake_useragent','lxml','scrapy','dotmap','pymysql','psycopq2','hjson','cchardet', 'tweepy', 'dtale', 'templates', 'snscrape','credentials','pyplotlib<3.3.0','requests_html','dateparser','demjson3','selenium','chromedriver_binary','requests_oauthlib','dateparser','feedparser','environ']:
         try:
             dist = pkg_resources.get_distribution(package)
             print('{} ({}) is installed'.format(dist.key, dist.version))
