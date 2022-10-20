bdc90229422c35a1c7801905822b4dfa46c29195
BgTrProject <85813523+BgTrProject@users.noreply.github.com>
2022-10-20 05:54:17 +0300

    Update trypackage.py

--- python/trypackage.py
+++ python/trypackage.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 def controlpackage():
-    for package in ['kivy','credentials', 'requests-oauthlib','pandas','dtale','templates','kivymd','selenium','bs4','requests-html','dateparser','demjson3','chromedriver-binary', 'Unidecode', 'feedparser', 'tldextract', 'Scrapy','dotmap','PyMySQL','psycopg2','elasticsearch','hjson','cchardet','webdriver-manager','html5lib']:   
+    for package in ['kivy','credentials', 'requests-oauthlib','pandas','dtale','templates','kivymd','selenium','bs4','requests-html','dateparser','demjson3','chromedriver-binary', 'Unidecode', 'feedparser', 'tldextract', 'Scrapy','dotmap','PyMySQL','psycopg2','elasticsearch','hjson','cchardet','webdriver-manager','html5lib','snscrape','tweepy','langdetect','ppscore','pyinstaller','readability','chardet']:   
         try:
             dist = pkg_resources.get_distribution(package)
             print('{} ({}) is installed'.format(dist.key, dist.version))
